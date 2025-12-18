4cd2793038602999db0cdc2fa4b9199c5d1e1426
zxyGo <zhang.xiaoyu@zxylucky.com>
2025-12-18 17:49:51 +0800

    feat(layout): 实现基础布局和菜单功能
    - 修改页面标题为"发布助手"
    - 添加全局样式确保全屏布局
    - 完善布局组件结构，添加头部和内容区域
    - 实现动态菜单渲染功能

--- src/layout/components/Menu/index.vue
+++ src/layout/components/Menu/index.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import { menuRouter } from '@/routers/modules/staticRouter'
 console.log(menuRouter)
+const menuItems = menuRouter.map(item => ({
+  path: item.path,
+  name: item.name,
+  meta: item.meta
+}))
 </script>
 <template>
   <t-menu>
     <template #logo>
       <div>logo</div>
     </template>
+    <t-menu-item
+      v-for="item in menuItems"
+      :key="item.path"
+      :path="item.path"
+      :name="item.name"
+      :meta="item.meta"
+    >
+      {{ item.meta.title }}
+    </t-menu-item>
   </t-menu>
 </template>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,9 +2,17 @@
 import Menu from './components/Menu/index.vue'
 </script>
 <template>
-  <t-layout>
+  <t-layout class="h-full w-full">
     <t-aside>
       <Menu />
     </t-aside>
+    <t-layout class="h-full">
+      <t-header>
+        <div>发布助手</div>
+      </t-header>
+      <t-content class="h-full">
+        <router-view></router-view>
+      </t-content>
+    </t-layout>
   </t-layout>
 </template>
